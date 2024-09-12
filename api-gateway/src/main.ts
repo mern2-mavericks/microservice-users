da7f8108a95b66dceaca88dcf88a7668f3a6abaa
Ilham Rusydi Abd <abd.ilhamrusydi@gmail.com>
2024-09-12 17:46:47 +0700

    fix: remove unnecessary configuration options

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,12 +1,9 @@
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
 
-const PORT = 3010;
+const PORT = 8010;
 const app = express();
 
-
-app.get("/", (_, res) => res.json({ message: "API Gateway" }));
-
-app.use("/users", createProxyMiddleware({ target: "http://users:8001" }))
+app.use("/users/", createProxyMiddleware({ target: "http://localhost:8000", logger: console }));
 
 app.listen(PORT, () => console.info(`listening on port ${PORT}`));
