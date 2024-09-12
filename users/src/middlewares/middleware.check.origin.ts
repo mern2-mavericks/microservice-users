c4fd7d8c524807c94a2536f9089eba72edf8332b
Ilham Rusydi Abd <abd.ilhamrusydi@gmail.com>
2024-09-12 13:22:25 +0700

    fix: add missing return

--- users/src/middlewares/middleware.check.origin.ts
+++ users/src/middlewares/middleware.check.origin.ts
@@ -1,13 +1,10 @@
 import { NextFunction, Request, Response } from "express";
 
-export async function middlewareCheckOrigin(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  if (req.headers.host === "localhost:3010") {
+export async function middlewareCheckOrigin(req: Request, res: Response, next: NextFunction) {
+  if (req.headers.host === "localhost:8010") {
     console.log("Origin request:", req.headers.host);
     next();
+    return;
   }
-  return res.status(403).send("you are not allowed access");
+  return res.status(403).send("you are not allowed access directly");
 }
