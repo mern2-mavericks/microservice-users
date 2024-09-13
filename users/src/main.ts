4dce2fea11807afc92c2dd0a2c2699b2d5db2c3c
Ilham Rusydi Abd <abd.ilhamrusydi@gmail.com>
2024-09-13 23:33:37 +0700

    add packages

--- users/src/main.ts
+++ users/src/main.ts
@@ -5,12 +5,22 @@ import cors from "cors";
 import { userRouter } from "./routes/user.route";
 import { connectDB } from "./utils/connectionDB";
 import { middlewareCheckOrigin } from "./middlewares/middleware.check.origin";
+import { rateLimit } from "express-rate-limit"
+import helmet from "helmet";
 
 dotenv.config();
 
 const app = express();
+const limiter = rateLimit({
+	windowMs: 1 * 60 * 1000, // 15 minutes
+	limit: 100,  // each IP can make up to 10 requests per `windowsMs` (5 minutes)
+	standardHeaders: true, // add the `RateLimit-*` headers to the response
+	legacyHeaders: false, // remove the `X-RateLimit-*` headers from the response
+	message: "too many request from this IP"
+})
+
 const port = process.env.PORT;
-app.use(cors({ origin: "http://localhost:8010" }));
+app.use(cors({ origin: "http://localhost:8010" }), limiter, helmet());
 app.use(middlewareCheckOrigin);
 
 connectDB();
